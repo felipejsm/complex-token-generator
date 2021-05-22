823e3495a7da6b16fd7a3e21fd9d158b26e6013e
felipejsm <felipejsm41@gmail.com>
2021-05-22 18:07:10 -0300

    init project

---
+++ src/main/java/com/nssp/complextokengenerator/ComplexTokenGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.nssp.complextokengenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexTokenGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplexTokenGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nssp/complextokengenerator/ComplexTokenGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nssp.complextokengenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexTokenGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
